ade8a861df6212775594f972c4b31ff8244705c9
sukmuzi <sukmuzi@naver.com>
2022-06-10 12:54:53 +0900

    날짜 형식 검증 annotation 추가

--- src/main/kotlin/com/example/mvc/controller/model/http/UserRequest.kt
+++ src/main/kotlin/com/example/mvc/controller/model/http/UserRequest.kt
@@ -1,5 +1,6 @@
 package com.example.mvc.controller.model.http
 
+import com.example.mvc.annotation.StringFormatDateTime
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import javax.validation.constraints.AssertTrue
@@ -28,15 +29,8 @@ data class UserRequest(
     @field:Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}\$") // 정규식 검증
     var phoneNumber: String? = null,
 
+    @field:StringFormatDateTime(pattern = "yyyy-MM-dd HH:mm:ss", message = "패턴이 올바르지 않습니다.")
     var createdAt: String? = null // yyyy-MM-dd HH:mm:ss  ex) 2022-06-09 18:00:00
 ) {
-    @AssertTrue(message = "생성일자의 패턴은 yyyy-MM-dd HH:mm:ss 여야 합니다.")
-    private fun isValidCreatedAt(): Boolean {
-        return try {
-            LocalDateTime.parse(this.createdAt, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-            true
-        } catch (e: Exception) {
-            false
-        }
-    }
+
 }
